26563d9eaf5e08d92ef96dc0c70b95606edc59a0
xellosPark <yukihiro81@naver.com>
2024-12-23 22:23:20 +0900

    git updata

---
+++ ub-project-app/src/boards/boards.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+//@nestjs/typeorm의 InjectRepository 데코레이터는 NestJS에서 TypeORM의 
+//리포지토리를 의존성 주입 방식으로 사용하기 위해 제공되는 도구입니다.
+//이 데코레이터는 주로 엔티티(Entity)를 데이터베이스에서 관리하고,
+//CRUD 작업을 처리하기 위해 사용됩니다.
+
+@Injectable()
+export class BoardsService {
+  
+
+}
